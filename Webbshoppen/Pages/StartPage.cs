3d1ccbedc0ba088468ac6747d1d8559ec8abfcfb
fruniia <annanilssonniia@gmail.com>
2023-01-10 10:07:06 +0100

    Added classes for shop
    Added classes needed for the shop with TODO-description

---
+++ Webbshoppen/Pages/StartPage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Webbshoppen.Pages
+{
+    internal class StartPage
+    {
+        //Välkomsttext
+        //Tre utvald produkter
+    }
+}
